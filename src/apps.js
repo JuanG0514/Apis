14b90ac20201366b356d40977caa6040229e4b8d
shir <juandavidgome336@gmail.com>
2025-05-30 11:21:32 -0500

    Apis

---
+++ src/apps.js
@@ -0,0 +1,17 @@
+const express = require (`express`);
+require(`express-async-errors`);
+const helmet =  require (`helmet`);
+const productoRoutes = require (`./routes/productoRoutes`);
+const errorHandler = require (`./middleware/errorHandler`);
+
+const app =  express();
+
+app.use(helmet());
+
+app.use(helmet.contentSecurityPolicy({
+        directives: {
+            defaultsrc: ["`self`"],
+            scriptSrc: ["`self`","unsafe-inline"],
+
+        }
+}))
